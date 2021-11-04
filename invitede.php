020616f9f41c3f14090a9ae812796c26a0c50f5e
YuliyaHit <nata.lychko@mail.ru>
2021-11-04 20:06:25 +0300

    4 правки
    добавила цветовую гамму, исправила категории выровняла отступы, сделала отдельные файлы стилей для страниц

--- invitede.php
+++ invitede.php
@@ -14,17 +14,18 @@ if (array_key_exists('lastname',$_REQUEST))
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Einladung</title>
-	<link rel="stylesheet" href="style.css?v=<?=rand(1,324234234)?>">
+	<link rel="stylesheet" href="stylede.css?v=<?=rand(1,324234234)?>">
 	<link rel="stylesheet" href="fonts.css">
-	<style> 
+	<!--<style> 
 		.form__choice, .form__decision {
 			padding-left: 0;
 		}
-	</style>
+	</style>-->
 </head>
 <body>
 	
-<div class="wrapper">	 
+<div class="wrapper">
+<div class = "s1">	 
 	<div class = "section1">
 		<h2>Christian & Alena</h2>
 		<picture>
@@ -39,25 +40,23 @@ if (array_key_exists('lastname',$_REQUEST))
 		$result = $mysqli->query("SELECT * FROM guests WHERE lastname = '$default_lastname'");
 			while ($row = $result->fetch_assoc()) {
 				if ($row['gender'] == 'man') {
-					echo "Sehr geehrter, ";
-				} else { echo "Sehr geehrte, ";}
+					echo "Lieber, ";
+				} else { echo "Liebe, ";}
 			}
 		$mysqli -> close();
 		echo $default_firstname.'!';?>
 	</h3>		
 
-	<p class= "invate-text">Wir freuen uns darüber mitzuteilen, dass unser wichtigstes Fest unseres Lebens – unsere Hochzeit, am <span class = "words">17.09.2022</span> stattfindet!<br><br>
+	<p class= "invate-text">Wir freuen uns dir mitteilen zu können, dass das wichtigste Fest unseres Lebens, unsere Hochzeit, am <span class = "words">17.09.2022</span> stattfinden wird!<br><br>
 
-	Wir laden dich ein, um die Freude dieses unvergessenen Tages mit uns zu teilen.
-	Wir warten auf dich am 
-	<span class = "words">17.09.2022</span> in der sonnigen Türkei!</p>
+	Wir laden dich herzlich ein, die Freude dieses unvergesslichen Tages mit uns zu teilen. Am <span class = "words">17.09.2022</span> erwarten wir dich in der sonnigen Türkei!</p>
 	
 	<picture>
-		<source srcset="images/foto2mb.jpg"  class="foto2" media="(max-width: 426px)">
+		<source srcset="images/foto2mb.jpg"  class="foto2" media="(max-width: 580px)">
 	    <img src="images/foto2dt.jpg" alt="" class="foto2">
 	</picture>
 
-	<p class = "timer-wait">Wir zählen jeden Tag bis zum Anfang unseres Festes, es fehlt noch</p>	
+	<p class = "timer-wait">Wir zählen jeden einzelnen Tag bis zum Beginn unserer Hochzeit runter</p>	
 
 	<div class="timer">
 		<div class="timer-numbers" id="timer">
@@ -82,11 +81,12 @@ if (array_key_exists('lastname',$_REQUEST))
 			</div>
 		</div>
 	</div>
-    <p class = "text">Uns ist es sehr wichtig, Antworten auf die folgenden Fragen zu bekommen...</p>
+    <p class = "text"> Uns ist es sehr wichtig, Antworten bis zum 30.11.2021 auf die folgenden Fragen zu bekommen...</p>
+</div>
 <div class = "wrapper-main__form">
 	<div class = "main__form">
 		<form action="invitede.php#form" method="POST" id ="form">
-	<?php
+<?php
 	include 'conection.php';
 
 		if($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -98,26 +98,26 @@ if (array_key_exists('lastname',$_REQUEST))
 		  	} */
 
 		  	if (empty($_POST["choice"])) {
-		    		$errors[1] = "Du hast die Frage nicht beantwortet...";
+		    		$errors[1] = "Eine Frage wurde nicht beantwortet...";
 		  	} 
 		  	if (empty($_POST["partner"])) {
 		    		$errors[2] = "Mit wem wirst du zu unserer Hochzeit kommen?";
 		  	} 
 
 		  	if (empty($_POST["food"])) {
-		    		$errors[3] = "Was bevorzugst du als Essen?";
+		    		$errors[3] = "Welches Essen bevorzugst du?";
 		  	}  
 
 		  	if (empty($_POST["drink"])) {
-		    		$errors[4] = "Was bevorzugst du trinken?";
+		    		$errors[4] = "Welche Getränke bevorzugst du?";
 		  	}   
 
 		  	if (empty($_POST["pfood"])) {
-		    		$errors[5] = "Was dein Partner/deine Partnerin als Essen bevorzugt";
+		    		$errors[5] = "Was deine Hochzeitsbegleitung beim Essen bevorzugt...";
 		  	}  
 
 		  	if (empty($_POST["pdrink"])) {
-		    		$errors[6] = "Was dein Partner/deine Partnerin zum Trinken bevorzugt";
+		    		$errors[6] = "Was deine Hochzeitsbegleitung bei den Getränken bevorzugt...";
 		  	}   
 
 		  	if (isset($_POST["choice"])) {
@@ -146,48 +146,49 @@ if (array_key_exists('lastname',$_REQUEST))
 
 			  	if($_POST["choice"] == "Не смогу прийти") {	
 
-					$mysqli->query("UPDATE guests SET answer = '$choice', partner = '0', meat = '$food[0]', fish = '$food[1]',  vegan = '$food[2]', any_food = '$food[3]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', brandy = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '', children = 'Нет', group_of_children1 = '0', group_of_children2 = '0', group_of_children3 = '0', date = Now() WHERE lastname = '$lastname'");
+					$mysqli->query("UPDATE guests SET answer = '$choice', partner = '0', meat = '$food[0]', fish = '$food[1]',  vegan = '$food[2]', seafood = '$food[3]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', whiskey = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '', children = 'Нет', group_of_children1 = '0', group_of_children2 = '0', group_of_children3 = '0', date = Now() WHERE lastname = '$lastname'");
 
 					if($count > 0) {
 						$mysqli->query("DELETE FROM guests WHERE lastname = '$default_lastname$default_firstname'");
 					} 
 					$errors = array();
-					echo "<span class = 'thanks'>"."Schade, dass du nicht kommen kannst"."</span>";
+					echo "<span class = 'thanks'>"."Sehr schade, dass du nicht anwesend sein kannst"."</span>";
 					$mysqli -> close();
 
-		  		}	else if(empty($errors) && $_POST['partner'] == 2) {
+		  		}	
+				else if(empty($errors) && $_POST['partner'] == 2) {
 
-						$mysqli->query("UPDATE guests SET answer = '$choice', partner = '$partner', meat = '$food[0]', fish = '$food[1]', any_food = '$food[3]', vegan = '$food[2]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', brandy = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '$commentFood', children = '$children', group_of_children1 = '$childBaby', group_of_children2 = '$childMedium', group_of_children3 = '$childTeenager', date = Now() WHERE lastname = '$lastname'");
+						$mysqli->query("UPDATE guests SET answer = '$choice', partner = '$partner', meat = '$food[0]', fish = '$food[1]', seafood = '$food[2]', vegan = '$food[3]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', whiskey = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '$commentFood', children = '$children', group_of_children1 = '$childBaby', group_of_children2 = '$childMedium', group_of_children3 = '$childTeenager', date = Now() WHERE lastname = '$lastname'");
 
 					/*Если партнер есть, то обновляем, если нет - добавляем */
 						if($count > 0) {
-							$mysqli->query("UPDATE guests SET answer = '$choice', meat = '$pfood[0]', fish = '$pfood[1]', any_food = '$pfood[3]', vegan = '$pfood[2]', beer = '$pdrinks[0]', white_wine = '$pdrinks[1]', red_wine = '$pdrinks[2]', champagne = '$pdrinks[3]', vodka = '$pdrinks[4]', brandy = '$pdrinks[5]', soft_drinks = '$pdrinks[6]', comment_food = '$pcommentFood', date = Now() WHERE lastname = '$plastname'");
-							echo "<span class = 'thanks'>"."Vielen Dank für deine Antwort!"."</span>";
+							$mysqli->query("UPDATE guests SET answer = '$choice', meat = '$pfood[0]', fish = '$pfood[1]', seafood = '$pfood[2]', vegan = '$pfood[3]', beer = '$pdrinks[0]', white_wine = '$pdrinks[1]', red_wine = '$pdrinks[2]', champagne = '$pdrinks[3]', vodka = '$pdrinks[4]', whiskey = '$pdrinks[5]', soft_drinks = '$pdrinks[6]', comment_food = '$pcommentFood', date = Now() WHERE lastname = '$plastname'");
+							echo "<span class = 'thanks'>"."Herzlichen Dank für deine Angaben!"."</span>";
 						} 
 						else {
 								$mysqli->query("INSERT INTO guests VALUES(null, '$pfirstname', '$plastname', '$choice', '0', '$pfood[0]', '$pfood[1]', '$pfood[2]', '$pfood[3]', '$pdrinks[0]', '$pdrinks[1]', '$pdrinks[2]', '$pdrinks[3]', '$pdrinks[4]', '$pdrinks[5]', '$pdrinks[6]', '$pcommentFood', 'Нет', '', '', '', Now(),'', '', '')");
 
-								echo "<span class = 'thanks'>"."Vielen Dank für deine Antwort!"."</span>";
+								echo "<span class = 'thanks'>"."Herzlichen Dank für deine Angaben!"."</span>";
 						}
 						$mysqli -> close();
 						
 				}
 				else if(empty($errors[2]) && empty($errors[3]) && empty($errors[4]) && $_POST['partner'] == 1) {
-						$mysqli->query("UPDATE guests SET answer = '$choice', partner = '$partner', meat = '$food[0]', fish = '$food[1]', any_food = '$food[3]', vegan = '$food[2]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', brandy = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '$commentFood', children = '$children', group_of_children1 = '$childBaby', group_of_children2 = '$childMedium', group_of_children3 = '$childTeenager', date = Now() WHERE lastname = '$lastname'");
+						$mysqli->query("UPDATE guests SET answer = '$choice', partner = '$partner', meat = '$food[0]', fish = '$food[1]', seafood = '$food[2]', vegan = '$food[3]', beer = '$drinks[0]', white_wine = '$drinks[1]', red_wine = '$drinks[2]', champagne = '$drinks[3]', vodka = '$drinks[4]', whiskey = '$drinks[5]', soft_drinks = '$drinks[6]', comment_food = '$commentFood', children = '$children', group_of_children1 = '$childBaby', group_of_children2 = '$childMedium', group_of_children3 = '$childTeenager', date = Now() WHERE lastname = '$lastname'");
 
 						/*Надо проверить есть ли строка  с партнером гостя по фамилии, если есть - удаляем*/
 						if($count > 0) {
 							$mysqli->query("DELETE FROM guests WHERE lastname = '$default_lastname$default_firstname'");
 						}
-						echo "<span class = 'thanks'>"."Vielen Dank für deine Antwort!"."</span>";
+						echo "<span class = 'thanks'>"."Herzlichen Dank für deine Angaben!"."</span>";
 						$mysqli -> close();
 			    }  
 			    else {
-			  		echo "<span class = 'check'>"."Es scheint, dass du irgendwelche Frage verpasst hast"."</span>";
+			  		echo "<span class = 'check'>"."Wahrscheinlich hast du eine Frage übersehen"."</span>";
 		  		}
 		  	}	
 		} 	
-	?>
+?>
 
 
 	<?php
@@ -200,38 +201,39 @@ if (array_key_exists('lastname',$_REQUEST))
 		if(isset($errors["1"])) {
 		echo "<span class = 'errors'>".$errors[1]."</span>";
 	}?>
+	<div class = "main__question">
 	<h4>Wirst du zu unserer Hochzeit kommen?</h4>
 		<div>
-			<label class="form__choice"><input type="radio"  class="form__decision" name="choice" value="Я прийду" >Ich bin dabei</label><br>	
-			<label  class="form__choice"><input type="radio" class="form__decision" name="choice" value="Скорее всего буду">Ich werde sehr wahrscheinlich dabei sein</label><br>
-			<label class="form__choice"><input type="radio" class="form__decision" name="choice" value="Не смогу прийти">Ich kann leider nicht kommen</label><br>
+			<label class="form__choice"><input type="radio"  class="form__decision" name="choice" value="Я прийду" >Ja, ich werde kommen</label><br>	
+			<label  class="form__choice"><input type="radio" class="form__decision" name="choice" value="Скорее всего буду">Ja, ich werde sehr wahrscheinlich kommen</label><br>
+			<label class="form__choice"><input type="radio" class="form__decision" name="choice" value="Не смогу прийти">Nein, ich kann leider nicht kommen</label><br>
 		</div>
-
+	</div>
 			<div class = "wrapper-form">
 				<div class= "guest_preferences">
 	<?php
 		if(isset($errors["4"])) {
 		echo "<span class = 'errors'>".$errors[4]."</span>";
 	}?>
-					<h4>Als Getränke bevorzuge ich...</h4>
+					<h4>Bei den Getränken bevorzuge ich...</h4>
 					<label class="form__choice"><input type="checkbox" name="drink[0]" value="1">Bier</label><br>
-					<label class="form__choice"><input type="checkbox" name="drink[1]" value="1">weißen Wein</label><br>
-					<label class="form__choice"><input type="checkbox" name="drink[2]" value="1">roten Wein</label><br>
+					<label class="form__choice"><input type="checkbox" name="drink[1]" value="1">Weißwein</label><br>
+					<label class="form__choice"><input type="checkbox" name="drink[2]" value="1">Rotwein</label><br>
 					<label class="form__choice"><input type="checkbox" name="drink[3]" value="1">Sekt</label><br>
 					<label class="form__choice"><input type="checkbox" name="drink[4]" value="1">Wodka</label><br>
-					<label class="form__choice"><input type="checkbox" name="drink[5]" value="1">Kognak</label><br>
+					<label class="form__choice"><input type="checkbox" name="drink[5]" value="1">Whiskey</label><br>
 					<label class="form__choice"><input type="checkbox" name="drink[6]" value="1">alkoholfreie Getränke</label><br>
 	<?php
 		if(isset($errors["3"])) {
 		echo "<span class = 'errors'>".$errors[3]."</span>";
 		}?>
-					<h4>Als Essen gefällt mir...</h4>
-					<label class="form__choice"><input type="checkbox" name="food[0]" value="1">Fleisch</label><br>
-					<label class="form__choice"><input type="checkbox" name="food[1]" value="1">Meeresfrüchte</label><br>
-					<label class="form__choice"><input type="checkbox" name="food[2]" value="1">Ich bin Vegetarianer</label><br>
-					<label class="form__choice"><input type="checkbox" name="food[3]" value="1">Mir gefällt alles!</label><br>
+					<h4>Beim Essen bevorzuge ich...</h4>
+					<label class="form__choice"><input type="checkbox" name="food[0]" value="1">Gerichte mit Fleisch</label><br>
+					<label class="form__choice"><input type="checkbox" name="food[1]" value="1">Gerichte mit Fisch</label><br>
+					<label class="form__choice"><input type="checkbox" name="food[2]" value="1">Gerichte mit Meeresfrüchten</label><br>
+					<label class="form__choice"><input type="checkbox" name="food[3]" value="1">vegetarische Gerichte</label><br>
 
-					<textarea class= "food_comment" placeholder="Wenn du Allergie, irgendwelche Beschränkungen im Essen oder in Getränken oder einfach Wünsche zur Organisation hast, dann teile bitte uns diese hier mit." name ="food-comment"></textarea>
+					<textarea class= "food_comment" placeholder="Wenn du Allergien, spezielle Essens- oder Getränkewünsche hast, dann teile uns dies hier bitte mit." name ="food-comment"></textarea>
 					</div>
 	<?php
 		if(isset($errors["2"])) {
@@ -253,7 +255,7 @@ if (array_key_exists('lastname',$_REQUEST))
 								}
 							?>
 							<hr>
-							<h4>Mein Partner/meine Partnerin trinkt...</h4>
+							<h4>Meine Hochzeitsbegleitung trinkt...</h4>
 							<label class="form__choice"><input type="checkbox" name="pdrink[0]" value="1">bier</label><br>
 							<label class="form__choice"><input type="checkbox" name="pdrink[1]" value="1">weißen Wein</label><br>
 							<label class="form__choice"><input type="checkbox" name="pdrink[2]" value="1">roten Wein</label><br>
@@ -266,26 +268,26 @@ if (array_key_exists('lastname',$_REQUEST))
 									echo "<span class = 'errors'>".$errors[5]."</span>";
 								}
 							?>
-							<h4>Als Essen bevorzugt mein Partner/meine Partnerin...</h4>
-							<label class="form__choice"><input type="checkbox" name="pfood[0]" value="1">Fleisch</label><br>
-							<label class="form__choice"><input type="checkbox" name="pfood[1]" value="1">Meeresfrüchte</label><br>
-							<label class="form__choice"><input type="checkbox" name="pfood[2]" value="1">Er/sie ist Vegetarianer/in </label><br>
-							<label class="form__choice"><input type="checkbox" name="pfood[3]" value="1">Meinem/er Partner/in gefällt alles!</label><br>
+							<h4>Beim Essen bevorzugt meine Hochzeitsbegleitung...</h4>
+							<label class="form__choice"><input type="checkbox" name="pfood[0]" value="1">Gerichte mit Fleisch</label><br>
+							<label class="form__choice"><input type="checkbox" name="pfood[1]" value="1">Gerichte mit Fisch</label><br>
+							<label class="form__choice"><input type="checkbox" name="pfood[2]" value="1">Gerichte mit Meeresfrüchten</label><br>
+							<label class="form__choice" id = pfood3><input type="checkbox" name="pfood[3]" value="1"><span></span></label><br>
 
-							<textarea class= "food_comment" placeholder="Wenn dein Partner/deine Partnerin Allergie, irgendwelche Beschränkungen im Essen oder Getränke, oder einfach wünsche zur Organisation der Hochzeit hat, dann teile uns diese bitte hier mit" name ="pcommentFood"></textarea>
+							<textarea class= "food_comment" placeholder="Wenn deine Hochzeitsbegleitung Allergien, spezielle Essens- oder Getränkewünsche hat, dann teile uns dies hier bitte mit." name ="pcommentFood"></textarea>
 							<hr>
 					</div>
 
-				<h4><label><input type="checkbox" class="form__children" name="children" value="Да">Mit mir kommen auch die Kinder:</label></h4><br>
+				<h4><label><input type="checkbox" class="form__children" name="children" value="Да">Mit mir kommen auch meine Kinder mit:</label></h4><br>
 			
 			
 				<div class = "wrapper-form-children">
 					<h6 class = "age">von 0 bis 5 Jahre</h6>
-					<input type="text" class="form__name" name="child-baby" placeholder="Gib bitte Anzahl von Kindern ein"><br>
+					<input type="text" class="form__name" name="child-baby" placeholder="Gib hier die Anzahl deiner Kinder ein"><br>
 					<h6 class = "age">von 6 bis 13 Jahre</h6>
-					<input type="text" class="form__name" name="child-medium" placeholder="Gib bitte Anzahl von Kindern ein"><br>
+					<input type="text" class="form__name" name="child-medium" placeholder="Gib hier die Anzahl deiner Kinder ein"><br>
 					<h6 class = "age">von 14 Jahre und älter </h6>
-					<input type="text" class="form__name" name="child-teenager" placeholder="Gib bitte Anzahl von Kindern ein"><br>
+					<input type="text" class="form__name" name="child-teenager" placeholder="Gib hier die Anzahl deiner Kinder ein"><br>
 				</div>	
 			</div>
 			
@@ -296,16 +298,30 @@ if (array_key_exists('lastname',$_REQUEST))
 		<img src="images/foto4dt.jpg" alt="Кристиан и Алена" class="foto3">
 	</div>
 </div>
-
-	<p class = "text-information">Später nach der Hochzeit wird hier unten der Link zu den Fotos und Hochzeitsfilm veröffentlicht. Die Mediadateien kann man zum Andenken speichern</p>
-	<div class = "photoreport">
-		<a class= "link" href="#">Video abspielen</a>
+<div class = "s2">
+	<div class = "wedding-style"> 
+		<p class = "wedding-style__text">Wir werden uns sehr freuen, wenn dein Hochzeitsoutfit den folgenden Farben ensprechen wird</p>
+		<div class = "colors"> 
+			<div class = "color" style = "background-color: #680400;"></div>
+			<div class = "color" style = "background-color: #8f5b4d;"></div>
+			<div class = "color" style = "background-color: #e7c2a7;"></div> 
+			<div class = "color" style = "background-color: #cda490;"></div>
+			<div class = "color" style = "background-color: #f1cbcb;"></div> 
+			<div class = "color" style = "background-color: #eee2d6;"></div>
+		</div>
+	</div>
+	<hr class = "line">
+	<p class = "text-information">Nach der Hochzeit findest du unter diesem Link Videos und Fotos von unserer Hochzeit. Diese stehen dir dann zum Download zur Verfügung.</p>
+		<div class = "photoreport">
+		<!-- <a class= "link" href="#">Смотреть видеоролик</a> -->
 		<a class= "link" href="#"><img src="images/play1.png" alt="" class="play"></a>
 		<a class= "link" href="#"><img src="images/load2.png" alt="" class="load"></a>
-		<a class= "link" href="#">Fotos herunterladen</a>
+		<!-- <a class= "link" href="#">Скачать фотоотчет</a> -->
 	</div>
+
 </div>
-<script src="script.js"></script>
+</div>
+<script src="scriptde.js"></script>
 	
 </body>
 </html>
